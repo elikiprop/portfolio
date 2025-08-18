01b102485ac69ee45f250235ef9d9cff2b9a46cd
Eli Kiprop <109068401+elikiprop@users.noreply.github.com>
2025-08-18 13:43:01 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,43 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+/**
+ * Contact Form Handler for Eli Kiprop Portfolio
+ * Updated: 2025
+ */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+// Your email address where messages will be sent
+$receiving_email_address = 'elikiprop42@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Get form data safely
+$name    = isset($_POST['name']) ? strip_tags($_POST['name']) : '';
+$email   = isset($_POST['email']) ? filter_var($_POST['email'], FILTER_SANITIZE_EMAIL) : '';
+$subject = isset($_POST['subject']) ? strip_tags($_POST['subject']) : 'Portfolio Contact';
+$message = isset($_POST['message']) ? htmlspecialchars($_POST['message']) : '';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// Validate required fields
+if (!$name || !$email || !$message) {
+    http_response_code(400);
+    echo "Please fill in all required fields.";
+    exit;
+}
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+// Email headers
+$headers   = "From: $name <$email>\r\n";
+$headers  .= "Reply-To: $email\r\n";
+$headers  .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Compose email
+$email_body  = "You have a new message from your portfolio contact form:\n\n";
+$email_body .= "Name: $name\n";
+$email_body .= "Email: $email\n";
+$email_body .= "Subject: $subject\n\n";
+$email_body .= "Message:\n$message\n";
 
-  echo $contact->send();
+// Send email
+if (mail($receiving_email_address, $subject, $email_body, $headers)) {
+    http_response_code(200);
+    echo "Thank you $name, your message has been sent successfully!";
+} else {
+    http_response_code(500);
+    echo "Sorry, something went wrong. Please try again later.";
+}
 ?>
